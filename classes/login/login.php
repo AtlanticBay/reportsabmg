28434783f0f15000034ad3530d5c3bb791d9f198
Huzaifa Zainuddin <huzaifazainuddin@A005753.local>
2017-03-22 08:58:07 -0500

    some more testing

--- classes/login/login.php
+++ classes/login/login.php
@@ -1,9 +1,91 @@
-<?php
-// Add password checks here if you want
+<?php 
+ // it will never let you open index(login) page if session is set
 if(isset($_POST['username'])) {
   $_SESSION['username'] = $_POST['username'];
   header("Location: index.php");
   exit;
 }
-include("../../templates/default/html/login_page.twig")
+ 
+ $error = false;
+  
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coding Cage - Login & Registration System</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<div class="container">
+
+ <div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+    
+     <div class="col-md-12">
+        
+         <div class="form-group">
+             <h2 class="">Login</h2>
+            </div>
+        
+         <div class="form-group">
+             <hr />
+            </div>
+            
+            <?php
+   if ( isset($errMSG) ) {
+    
+    ?>
+    <div class="form-group">
+             <div class="alert alert-danger">
+    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+             </div>
+                <?php
+   }
+   ?>
+            
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+             <input type="email" name="email" class="form-control" placeholder="Your Email" value="<?php echo $email; ?>" maxlength="40" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+            
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="password" name="pass" class="form-control" placeholder="Your Password" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div>
+            
+            <div class="form-group">
+             <hr />
+            </div>
+            
+            <div class="form-group">
+             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+            </div>
+            
+            <div class="form-group">
+             <hr />
+            </div>
+            
+            <div class="form-group">
+             <a href="signup.php">Register Here</a>
+            </div>
+        
+        </div>
+   
+    </form>
+    </div> 
+
+</div>
+
+</body>
+</html>
+<?php ob_end_flush(); ?>
