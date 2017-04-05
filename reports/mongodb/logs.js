b002a9bfe67413848afe3c0a2ae920c22a238c26
Huzaifa Zainuddin <huzaifazainuddin@A005753.local>
2017-04-05 17:35:52 -0500

    fixed taskname

--- reports/mongodb/logs.js
+++ reports/mongodb/logs.js
@@ -5,7 +5,26 @@
 // FILTER: {filter: "date", column: "Error Log Date", params: {"format": "m-d-Y h:i A"}}
 // VARIABLE : {name:"str", display: "Start Date", type:"date"}
 // VARIABLE : {name: "endr", display: "End Date", type: "date"}
-// VARIABLE : {name:"taskname", display: "Integration Task Name", type:"select", options:["All Tasks (No Filter)","Core_BYTE-Rep_SF-Prod_Condition","Core_BYTE-Rep_SF-Prod_Contact_Employee","Core_BYTE-Rep_SF-Prod_ContactVersion","Core_BYTE-Rep_SF-Prod_Loan""Core_BYTE-Rep_SF-Prod_LoanProperty","Core_BYTE-Rep_SF-Prod_LoanRelationship_AdditionalParty","Core_BYTE-Rep_SF-Prod_LoanRelationship_Borrowers","Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData","Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData_Clean_Contact","Core_BYTE-Rep_SF-Prod_LoanRelationship_Party","Core_BYTE-Rep_SF-Prod_ProductionUnit","Core_BYTE-Rep_SF-Prod_Status","Core_BYTE-Rep_SF-Prod_Term","Core_BYTE-Rep_SF-Prod_Term_Update","Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly","Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly_Update"]}
+// VARIABLE : {name:"taskname", display: "Integration Task Name", type:"select", options:[
+//                              "All Tasks (No Filter)",
+//                              "Core_BYTE-Rep_SF-Prod_Condition",
+//                              "Core_BYTE-Rep_SF-Prod_Contact_Employee",
+//                              "Core_BYTE-Rep_SF-Prod_ContactVersion",
+//                              "Core_BYTE-Rep_SF-Prod_Loan",
+//                              "Core_BYTE-Rep_SF-Prod_LoanProperty",
+//                              "Core_BYTE-Rep_SF-Prod_LoanRelationship_AdditionalParty",
+//                              "Core_BYTE-Rep_SF-Prod_LoanRelationship_Borrowers",
+//                              "Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData",
+//                              "Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData_Clean_Contact",
+//                              "Core_BYTE-Rep_SF-Prod_LoanRelationship_Party",
+//                              "Core_BYTE-Rep_SF-Prod_ProductionUnit",
+//                              "Core_BYTE-Rep_SF-Prod_Status",
+//                              "Core_BYTE-Rep_SF-Prod_Term",
+//                              "Core_BYTE-Rep_SF-Prod_Term_Update",
+//                              "Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly",
+//                              "Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly_Update"
+//                              ]
+//                      }
 
 
 // Query collection
