f1a6f65805b551d187d014578a24747d32c87345
Huzaifa Zainuddin <huzaifazainuddin@A005753.local>
2017-04-05 16:10:02 -0500

    updated query tasknames

--- reports/mongodb/logs.js
+++ reports/mongodb/logs.js
@@ -5,8 +5,7 @@
 // FILTER: {filter: "date", column: "Error Log Date", params: {"format": "m-d-Y h:i A"}}
 // VARIABLE : {name:"str", display: "Start Date", type:"date"}
 // VARIABLE : {name: "endr", display: "End Date", type: "date"}
-// VARIABLE : {name:"taskname", display: "Integration Task Name", type:"select", options:["All Tasks (No Filter)","Prod_st_BYTEtoSF_ContactVersion","Prod_st_BYTEtoSF_Contact_Employee","Prod_st_BYTEtoSF_Loan","Prod_st_BYTEtoSF_LoanRelationship_AdditionalParty","Prod_st_BYTEtoSF_LoanRelationship_Borrowers","Prod_st_BYTEtoSF_LoanRelationship_FileData","Prod_st_BYTEtoSF_LoanRelationship_FileData_Clean_Contact","Prod_st_BYTEtoSF_LoanRelationship_Party","Prod_st_BYTEtoSF_LoanProperty","Prod_st_BYTEtoSF_Status","Prod_st_BYTEtoSF_Condition","Prod_st_BYTEtoSF_Term","Prod_st_BYTEtoSF_Term_Update","Prod_st_BYTEtoSF_Term_Update"]}
-
+// VARIABLE : {name:"taskname", display: "Integration Task Name", type:"select", options:["All Tasks (No Filter)","Core_BYTE-Rep_SF-Prod_Condition","Core_BYTE-Rep_SF-Prod_Contact_Employee","Core_BYTE-Rep_SF-Prod_ContactVersion","Core_BYTE-Rep_SF-Prod_Loan""Core_BYTE-Rep_SF-Prod_LoanProperty","Core_BYTE-Rep_SF-Prod_LoanRelationship_AdditionalParty","Core_BYTE-Rep_SF-Prod_LoanRelationship_Borrowers","Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData","Core_BYTE-Rep_SF-Prod_LoanRelationship_FileData_Clean_Contact","Core_BYTE-Rep_SF-Prod_LoanRelationship_Party","Core_BYTE-Rep_SF-Prod_ProductionUnit","Core_BYTE-Rep_SF-Prod_Status","Core_BYTE-Rep_SF-Prod_Term","Core_BYTE-Rep_SF-Prod_Term_Update","Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly","Core_BYTE-Rep_BYTE-Rep_LogRun_Hourly_Update"]}
 
 
 // Query collection
