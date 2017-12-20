0727a1ddb4b52aaf1b5bb4ce011208aa3865a087
Huzaifa Zainuddin <huzaifazainuddin@atlanticbay.com>
2017-12-20 14:32:10 -0600

    add pivots for whatif

--- reports/whatif/currentassignments_summarized.sql
+++ reports/whatif/currentassignments_summarized.sql
@@ -2,6 +2,6 @@
 -- This is the first part of the whatif report creation:
 -- Summarizes number of loans each team member has been a part of and touched during a 12 month period. Run pivot based on the picklist given and attach each result as  sheet to an excel workbook
 -- OPTION: { database: "whatif" }
--- VARIABLE: { name: "input", display: "Type", type: "select", options: ["LoanOfficer","CurrentUnderwriter","CurrentProcessor","CurrentCloser"]}
+-- VARIABLE: { name: "input", display: "Type", type: "select", options: ["LoanOfficer","CurrentUnderwriter","CurrentProcessor","CurrentCloser","Underwriter_Byte_User_Name","Processor_Byte_User_Name","Closer_Byte_User_Name","Title_Review_User_Name"]}
 
 call `whatif`.pivot("{{ input }}")
